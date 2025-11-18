27f52e3136e03dce49eb47f1db9f58a3fcaa0c6b
Navy316 <gillbg16@gmail.com>
2025-11-17 19:01:31 -0600

    Refactor MIPS instruction decoding structure

--- main_files/mips_parser.cpp
+++ main_files/mips_parser.cpp
@@ -14,7 +14,8 @@ struct DecodedInstruction {
     uint8_t funct;
     int16_t immediate;  // signed for sign extension
     uint32_t address;
-}
+};
+
 DecodedInstruction decode(uint32_t instruction) {
     DecodedInstruction decoded;
     
@@ -41,6 +42,7 @@ DecodedInstruction decode(uint32_t instruction) {
 	}
 	return decoded;
 }
+
 
 
 
